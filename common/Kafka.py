cb1c31b1fadf2532e64e145cb6ee4a0f661fe28e
Pascual Juan Sánchez <pjs8@alu.ua.es>
2021-11-12 16:51:42 +0100

    Desacoplado Kafka

--- common/Kafka.py
+++ common/Kafka.py
@@ -4,6 +4,7 @@ from kafka import KafkaConsumer
 from json import dumps
 from json import loads
 import numpy as np
+from time import sleep
 
 # TOPICS DEL ZOOKEEPER
 TOPIC_MOVIMIENTO = 'movimiento'
@@ -15,53 +16,75 @@ TOPIC_PASEN = "pasen"
 # CONSUMIDOR JSON
 def definirConsumidorJSON(host, port, topic):
     print("Conectando consumidor Kafka...")
-    consumidor = None
-    while(consumidor == None):
-        consumidor = KafkaConsumer(
-            topic,
-            bootstrap_servers=[host + ':' + port],
-            auto_offset_reset='latest',
-            value_deserializer=lambda x:
-            loads(x.decode('utf-8'))  
-        )
+    conexion = False
+    while(conexion == False):
+        try:
+            consumidor = KafkaConsumer(
+                topic,
+                bootstrap_servers=[host + ':' + port],
+                auto_offset_reset='latest',
+                value_deserializer=lambda x:
+                loads(x.decode('utf-8'))  
+            )
+            conexion = True
+        except Exception as e:
+            print("No se ha podido conectar Kafka en " + host + ":" + port)
+            sleep(5)
 
     return consumidor
 
 ## PRODUCTOR JSON
 def definirProductorJSON(host, port):
     print("Creando productor Kafka...")
-    productor = None
-    while(productor == None):
-        productor =  KafkaProducer(
-            bootstrap_servers=[host + ':' + port],
-            value_serializer=lambda x:
-            dumps(x).encode('utf-8')
-        )
+    conexion = False
+    while(conexion == False):
+        try:
+            productor =  KafkaProducer(
+                bootstrap_servers=[host + ':' + port],
+                value_serializer=lambda x:
+                dumps(x).encode('utf-8')
+            )
+            conexion = True
+        except Exception as e:
+            print("No se ha podido conectar Kafka en " + host + ":" + port)
+            sleep(5)
+
     return productor
 
 # CONSUMIDOR EN BYTES
 def definirConsumidorBytes(host, port, topic):
     print("Conectando consumidor Kafka...")
-    consumidor = None
-    while(consumidor == None):
-        consumidor = KafkaConsumer(
-            topic,
-            bootstrap_servers=[host + ':' + port],
-            auto_offset_reset='latest',
-            value_deserializer=lambda x:
-            np.frombuffer(x, dtype='U3').reshape(20,20)  
-        )
+    conexion = False
+    while(conexion == False):
+        try:
+            consumidor = KafkaConsumer(
+                topic,
+                bootstrap_servers=[host + ':' + port],
+                auto_offset_reset='latest',
+                value_deserializer=lambda x:
+                np.frombuffer(x, dtype='U3').reshape(20,20)  
+            )
+            conexion = True
+        except Exception as e:
+            print("No se ha podido conectar Kafka en " + host + ":" + port)
+            sleep(5)
 
     return consumidor
 
 ## PRODUCTOR EN BYTES
 def definirProductorBytes(host, port):
     print("Creando productor Kafka...")
-    productor = None
-    while(productor == None):
-        productor =  KafkaProducer(
-            bootstrap_servers=[host + ':' + port],
-            value_serializer=lambda x:
-            x.tobytes()
-        )
+    conexion = False
+    while(conexion == False):
+        try:
+            productor =  KafkaProducer(
+                bootstrap_servers=[host + ':' + port],
+                value_serializer=lambda x:
+                x.tobytes()
+            )
+            conexion = True
+        except Exception as e:
+            print("No se ha podido conectar Kafka en " + host + ":" + port)
+            sleep(5)
+    
     return productor
