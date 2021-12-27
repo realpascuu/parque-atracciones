571882eb6ecd99cfa63247030dec731bbc7f4575
Pascual Juan Sánchez <pjs8@alu.ua.es>
2021-12-27 22:57:54 +0100

    API_Registry en funcionamiento

--- api_registry/main.py
+++ api_registry/main.py
@@ -4,7 +4,7 @@ import mysql.connector
 import sys
 
 sys.path.append('..')
-from FWQ_Registry import actualizar
+from FWQ_Registry import actualizar, registrarse, login
 
 
 app = Flask('server')
@@ -15,10 +15,45 @@ CORS(app)
 @app.route('/')
 
 def hello():
-    return  jsonify({ 'message' : "Hello world!"})
+    return  jsonify({ 'message' : "Hello world!" })
 
+# ruta que hace el registro del usuario
+@app.route('/usuarios', methods= ["POST"])
+def crearUsuario():
+    request_data = request.get_json()
+    username = request_data['username']
+    password = request_data['password']
 
+    alias = username[0:2]
 
+    message = registrarse(username, password, alias)
+    return jsonify({ 'message': message})
+
+# ruta que realiza la actualización del usuario
+@app.route('/usuarios/<string:oldUsername>', methods=['PUT'])
+def actualizarUsuario(oldUsername):
+    request_data = request.get_json()
+    newUsername = request_data['username']
+    password = request_data['password']
+
+    message = actualizar(oldUsername, newUsername, password)
+
+    return jsonify({'message': message})
+
+# ruta qye realiza el login de un usuario
+@app.route('/login', methods=['POST'])
+def loginUsuario():
+    request_data = request.get_json()
+    username = request_data['username']
+    password = request_data['password']
+
+    message, username, alias = login(username, password)
+
+    return jsonify({
+        'message': message,
+        'username': username,
+        'alias': alias
+    })
 
 if __name__ == '__main__':
     app.run(debug=True, port=5005, ssl_context=('./ssl/cert.pem', './ssl/key.pem'))
