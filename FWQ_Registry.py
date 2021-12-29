77eb8fded086a90dfa4992549d71549b76f89044
raulbr99 <raulelche14@gmail.com>
2021-12-29 02:26:35 +0100

    Auditoria realizada

--- FWQ_Registry.py
+++ FWQ_Registry.py
@@ -29,7 +29,7 @@ LLAMADA_REGISTRY = "python3 FWQ_Registry.py <puerto_escucha>"
 _cleanup_coroutines = []
 
 
-def registrarse(username, password, id):
+def registrarse(username, password, id, ip_cliente):
     try:
         mydb = mysql.connector.connect(
         host=host_BD,
@@ -47,7 +47,7 @@ def registrarse(username, password, id):
         if data:
             message = "El usuario ya existe"
             existe = True
-            #subirEvento(p)
+            subirEvento(ip_cliente,"REGISTRO","ERROR: El usuario " + username + " ya existe") 
         if not existe:
             passHash,salt = obtenerHash(password)
             sql = "INSERT INTO usuarios(username, password, salt, alias) VALUES (%s, %s, %s, %s)"
@@ -56,7 +56,7 @@ def registrarse(username, password, id):
 
             mydb.commit()
             message = "REGISTRO EXITOSO!!"
-            logging.info("Se ha registrado el usuario " + username)
+            subirEvento(ip_cliente,"REGISTRO","INFO: Se ha registrado el usuario " + username) 
 
         return message
     except Exception as e:
@@ -64,7 +64,7 @@ def registrarse(username, password, id):
         message = "No se ha podido establecer conexión con BD en " + host_BD + ":3306";
         return e
 
-def login(username, password, ):
+def login(username, password, ip_cliente):
     try:
         mydb = mysql.connector.connect(
         host=host_BD,
@@ -81,11 +81,11 @@ def login(username, password, ):
         data = mycursor.fetchall()
         if not data:
             existe = False
+            subirEvento(ip_cliente,"LOGIN","INFO: Se ha logueado el usuario " + username) 
             message = "El usuario " + username + " no existe"
             usernameLog = username
             alias = username[0:2]
         else:
-            logging.info("Usuario correcto")
             usernameLog = data[0][0]
             alias = data[0][1]
 
@@ -97,8 +97,9 @@ def login(username, password, ):
             saltBD = data[0][1]
             if hashIgual(passwordBD, saltBD, password):
                 message ="Autentificación exitosa!!"
-                logging.info("Se ha iniciado sesión con el usuario " + username)
+                subirEvento(ip_cliente,"LOGIN","INFO: Se ha logueado el usuario " + username) 
             else:
+                subirEvento(ip_cliente,"LOGIN","INFO: Se ha intentado loguear el usuario " + username + " con la contraseña incorrecta") 
                 message = "Contraseña incorrecta"
 
         return (message, usernameLog, alias)
@@ -108,7 +109,7 @@ def login(username, password, ):
         return (message, "", "")
 
 
-def actualizar(oldUsername, newUsername, password):
+def actualizar(oldUsername, newUsername, password, ip_cliente):
     try:
         mydb = mysql.connector.connect(
         host=host_BD,
@@ -125,9 +126,9 @@ def actualizar(oldUsername, newUsername, password):
             mycursor.execute(queryUser)
             mydb.commit()
             message = "USUARIO ACTUALIZADO!"
+            subirEvento(ip_cliente,"ACTUALIZAR","INFO: Se ha actualizado el usuario " + oldUsername + " a " + newUsername) 
         except mysql.connector.Error as err:
-            logging.info("Error: No se puede actualizar el usuario")
-            message = "Error: No se puede actualizar el usuario"
+            subirEvento(ip_cliente,"ACTUALIZAR","ERROR: No se puede actualizar el usuario: " + err)
 
         return message
     except Exception as e:
@@ -136,28 +137,20 @@ def actualizar(oldUsername, newUsername, password):
         return message
 class Register(register_pb2_grpc.RegisterServicer):
     async def doRegister(self, request: register_pb2.UserRequest, context: grpc.aio.ServicerContext) -> register_pb2.UserReply:
-        logging.info('Se quiere registrar el usuario ' + request.username + ' con la contraseña ' + request.password)
-      
-        message = registrarse(request.username, request.password, request.id)
-
+        subirEvento(ip_cliente(context),"REGISTRO","INFO: Se quiere registrar el usuario " + request.username + " con la contraseña " + request.password)      
+        message = registrarse(request.username, request.password, request.id, ip_cliente(context))
         return register_pb2.UserReply(message=message)
-
 
 class Login(register_pb2_grpc.LoginServicer):
     async def doLogin(self, request: register_pb2.UserPedido, context: grpc.aio.ServicerContext) -> register_pb2.UserRespuesta:
-        logging.info("Se quiere loguear el usuario " + request.username)
-        ip_cliente = context.peer()
-        indice1 = ip_cliente.index('[')
-        indice2 = ip_cliente.index(']')
-        ip_cliente = ip_cliente[indice1+1:indice2]
-        print(ip_cliente)
-        (message, usernameLog, alias) = login(request.username, request.password)
+        subirEvento(ip_cliente(context),"LOGIN","INFO: Se quiere loguear el usuario" + request.username)        
+        (message, usernameLog, alias) = login(request.username, request.password,ip_cliente(context))
         return register_pb2.UserRespuesta(message=message, username=usernameLog, alias=alias)
 
 class Update(register_pb2_grpc.UpdateServicer):
     async def doUpdate(self, request: register_pb2.UserToChange, context: grpc.aio.ServicerContext) -> register_pb2.UserUpdate:
-        logging.info("Se quiere modificar el usuario " + request.oldUsername)
-        message = actualizar(request.oldUsername, request.newUsername, request.password)
+        subirEvento(ip_cliente(context),"ACTUALIZAR", "INFO: Se quiere modificar el usuario " + request.oldUsername)
+        message = actualizar(request.oldUsername, request.newUsername, request.password, ip_cliente(context))
         return register_pb2.UserUpdate(message=message)
 
 
@@ -189,12 +182,18 @@ def subirEvento(IP, accion, descripcion):
     mycursor = mydb.cursor()
 
     fecha = datetime.now()
-    query = "INSERT INTO Eventos(fecha,IP_error,acción,descripción) VALUES (%s,%s,%s,%s)"
+    query = "INSERT INTO Eventos(fecha,IP,acción,descripción) VALUES (%s,%s,%s,%s)"
     val = (fecha,IP, accion, descripcion)
 
     mycursor.execute(query,val)
     mydb.commit()
 
+def ip_cliente(context):
+    ip_cliente = context.peer()
+    indice1 = ip_cliente.index('[')
+    indice2 = ip_cliente.index(']')
+    ip_cliente = ip_cliente[indice1+1:indice2]
+    return ip_cliente
 
 def obtenerHash(password):
     characters = string.ascii_letters + string.digits + string.punctuation
@@ -230,12 +229,13 @@ async def serve(port) -> None:
     
     server.add_secure_port(listen_addr, server_credentials)
        
+    subirEvento(listen_addr, "SERVER", "Encendiendo servidor...")   
     logging.info("Starting server on %s", listen_addr)
     await server.start() 
 
 # Módulo para apagar correctamente el registry
     async def server_apagado():
-        logging.info("Cerrando...")
+        subirEvento(listen_addr,"SERVER", "Apagando servidor...")
 
     _cleanup_coroutines.append(server_apagado())
     await server.wait_for_termination()
